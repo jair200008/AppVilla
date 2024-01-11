0fdbe9de622792c31de3d1ea447c03f74b82c7b5
jair200008 <jairgaona60@gmail.com>
2024-01-11 09:55:48 -0500

    API CRUD usuarios

--- backend/src/controllers/users.controller.js
+++ backend/src/controllers/users.controller.js
@@ -1,17 +1,42 @@
 const userCtrl = {};
 const Users = require('../models/Users');
 
+// CRUD MONGO CONSULTAR PARA CREAR USUARIOS 
+
 userCtrl.getUsers = async(req, res) => {   
     const user = await Users.find();
     res.json(user)
 }
 
-userCtrl.createUser = (req, res) => {
-    console.log(req.body);
+userCtrl.getUser = async (req, res) => {
+    const user = await Users.findById(req.params.id);
+    res.json(user)
+}
+
+userCtrl.createUser = async (req, res) => {
+    const { name, email, password } = req.body;
+    const newUser = new Users({
+        name,
+        email,
+        password
+    });
+    await newUser.save();
     res.json({message: 'User saved'})
 }
 
-userCtrl.deleteUser = (req, res) => res.json({message: []})
+userCtrl.updateUser = async (req, res) => {
+    const {name, email, password} = req.body;
+    await Users.findByIdAndUpdate(req.params.id, {
+        name,
+        email,
+        password
+    })
+    res.json({message: "User Update"})
+}
 
+userCtrl.deleteUser = async (req, res) => {
+    await Users.findByIdAndDelete(req.params.id);
+    res.json({message: 'User Deleted'})
+}
 
 module.exports = userCtrl
--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,11 +1,13 @@
 const {Router} = require('express');
 const router = Router();
 
-const {getUsers, createUser, deleteUser} = require('../controllers/users.controller')
+const {getUsers, createUser, deleteUser, getUser, updateUser} = require('../controllers/users.controller')
 router.route('/')
     .get(getUsers)
     .post(createUser)
 
 router.route('/:id')
     .delete(deleteUser)
-    module.exports = router;
+    .get(getUser)
+    .put(updateUser)
+module.exports = router;
