6099479c1fe5c4d7a691f1feba40527b9c90dae6
Juan Manuel Malaver <juanmalaver1511@gmail.com>
2024-01-11 12:57:47 -0500

    solucionComponentes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,17 +1,13 @@
-import './App.css';
-import LoginButton from './components/login';
-import LogoutButton from './components/logout';
-import { useEffect } from 'react';
+// App.js
+import React, { useEffect } from 'react';
 import { gapi } from 'gapi-script';
-import { BrowserRouter as Router , Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/login';
-import LoginES from './components/loginES'; 
+import LoginES from './components/loginES';
 import Home from './components/home';
 import Error from './components/error';
 import FormRegister from './components/formRegister';
 import PageRestaurant from './components/restaurant/componentRestaurant';
-import Prueba from './components/restaurant/prueba';
-
 
 const clientId = "72464211646-0hiu8ls489tmfvm6dqcn8ut4323hrith.apps.googleusercontent.com";
 
@@ -31,17 +27,16 @@ function App() {
 
   return (
     <div className="App">
-    <Router>
+      <Router>
         <Routes>
-          <Route  exact path="/" element={<Login />} />
-          <Route  path="/loginES" element={<LoginES />} />
-          <Route  path="/home" element={<Home />} />
-          <Route  path="/formRegister" element={<FormRegister />} />
-          <Route  path="/componentRestaurant" element={<PageRestaurant />} />
-          <Route  path="/prueba" element={<Prueba/>}/>
-          <Route  path="/error" component={Error} />
+          <Route path="/" element={<Login />} />
+          <Route path="/loginES" element={<LoginES />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/formRegister" element={<FormRegister />} />
+          <Route path="/componentRestaurant/*" element={<PageRestaurant />} />
+          <Route path="/error" element={<Error />} />
         </Routes>
-    </Router>
+      </Router>
     </div>
   );
 }
--- Frontend/src/components/restaurant/componentRestaurant.jsx
+++ Frontend/src/components/restaurant/componentRestaurant.jsx
@@ -1,22 +1,38 @@
 import React from "react";
-import {Link} from 'react-router-dom';
-function PageRestaurant(){
-    
-    return (
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Inicio</Link>
-            </li>
-            <li>
-              <Link to="/about">Acerca de</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contacto</Link>
-            </li>
-          </ul>
-        </nav>
-      );
-}
+import { Link, Route, Routes } from "react-router-dom";
+import Prueba from "./prueba";
+import Home from "../home";
+
+const PageRestaurant = () => {
+  return (
+    <div>
+      <div className="navContainer">
+        <div className="iconHouse">
+          <Link to="/home">
+            <img src="/img/casa.png" alt="" />
+          </Link>
+        </div>
+        <ul className="navOptions">
+          <li>
+            <Link to="/componentRestaurant/prueba">Opcion 1</Link>
+          </li>
+          <li>
+            <Link to="/componentRestaurant/prueba">Opcion 2</Link>
+          </li>
+          <li>
+            <Link to="/componentRestaurant/prueba">Llamar componente</Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h1>Bienvenido a name</h1>
+      </div>
+      <Routes>
+        <Route path="/" element={<div>Informacion propia de la pagina de restaurant</div>} />
+        <Route path="/prueba" element={<Prueba />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default PageRestaurant;
