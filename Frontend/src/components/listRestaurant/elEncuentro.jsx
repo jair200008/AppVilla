9042321ba1f2c3067934fc56b5e59bf6e3174690
Juan Manuel Malaver <juanmalaver1511@gmail.com>
2024-01-12 10:54:26 -0500

    lorena

--- Frontend/src/components/listRestaurant/elEncuentro.jsx
+++ Frontend/src/components/listRestaurant/elEncuentro.jsx
@@ -6,6 +6,12 @@ export default class ElEncuentro extends Component{
         return(
             <div className="containerText">
                 <h1>Bienvenido al Restaurante El Encuentro</h1>
+                <li>
+                    opcion1
+                </li>
+                <li>
+                    opcion2
+                </li>
                 <div className="imagenCarta">
                     <img src="/img/carta.jpg" alt="Restaurante el Encuentro"/>
                 </div>
