ab2012eeed504a32f1819a82105d532e64732778
Juan Manuel Malaver <juanmalaver1511@gmail.com>
2024-01-22 20:53:20 -0500

    add Carrito

--- Frontend/src/components/listRestaurant/elEncuentro.jsx
+++ Frontend/src/components/listRestaurant/elEncuentro.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-
+import Filtro from "../listRestaurant/filtro";
+import { Link } from "react-router-dom";
 class ElEncuentro extends Component {
+
   showModal = (title, imageSrc, price, description) => {
     document.getElementById("modalTitle").textContent = title;
     document.getElementById("modalImage").src = imageSrc;
@@ -16,12 +18,27 @@ class ElEncuentro extends Component {
   addToCart = () => {
     let cantidad = document.getElementById("quantity").value;
     if (cantidad > 0) {
+      let producto = {
+        title: document.getElementById("modalTitle").textContent,
+        imageSrc: document.getElementById("modalImage").src,
+        price: document.getElementById("modalPrice").textContent,
+        quantity: cantidad,
+      };
+
+      // Obtener carrito actual del localStorage
+      let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+      // Agregar el nuevo producto al carrito
+      carrito.push(producto);
+
+      // Guardar el carrito actualizado en el localStorage
+      localStorage.setItem("carrito", JSON.stringify(carrito));
       alert("Producto agregado al carrito: " + cantidad);
-      
+      this.closeModal();
+
     } else {
       alert("Cantidad inválida");
     }
-    this.closeModal();
   };
 
   state = {
@@ -33,126 +50,122 @@ class ElEncuentro extends Component {
       <div className="containerText">
         <h1>Bienvenido al Restaurante El Encuentro</h1>
         <div className="menu-container">
-        <div
-          className="menu-card"
-          onClick={() =>
-            this.showModal(
-              "Postre",
-              "/img/postre.jpg",
-              "$15.99",
-              "Descripcion del plato. Lorem ipsum"
-            )
-          }
-        >
-          <img src="/img/postre.jpg" alt="" />
-          <div className="menu-details">
-            <h2>Postre</h2>
-            <p>
-              Descripción del plato 1. Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit.
-            </p>
-            <p className="price">$15.99</p>
+          <div
+            className="menu-card"
+            onClick={() =>
+              this.showModal(
+                "Postre",
+                "/img/postre.jpg",
+                "$15.99",
+                "Descripcion del plato. Lorem ipsum"
+              )
+            }
+          >
+            <img src="/img/postre.jpg" alt="" />
+            <div className="menu-details">
+              <h2>Postre</h2>
+              <p>
+                Descripción del plato 1. Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit.
+              </p>
+              <p className="price">$15.99</p>
+            </div>
+          </div>
+          {/*----------------*/}
+          <div
+            className="menu-card"
+            onClick={() =>
+              this.showModal(
+                "Burritos Mexicanos",
+                "/img/tacos.jpg",
+                "$15.99",
+                "Descripción del plato 1. Lorem ipsum dolor sit amet, consectetur"
+              )
+            }
+          >
+            <img src="/img/tacos.jpg" alt="" />
+            <div className="menu-details" id="comidaRapida">
+              <h2>Burritos Mexicanos</h2>
+              <p>
+                Descripción del plato 1. Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit.
+              </p>
+              <p className="price">$15.99</p>
+            </div>
+          </div>
+          {/*----------------*/}
+          <div className="menu-card">
+            <img src="/img/pizza.jpg" alt="" />
+            <div className="menu-details">
+              <h2>Name product</h2>
+              <p>
+                Descripción del plato 1. Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit.
+              </p>
+              <p className="price">$15.99</p>
+            </div>
+          </div>
+
+          <div className="menu-card">
+            <img src="/img/hamburguesa.png" alt="" />
+            <div className="menu-details">
+              <h2>Name product</h2>
+              <p>
+                Descripción del plato 1. Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit.
+              </p>
+              <p className="price">$15.99</p>
+            </div>
+          </div>
+
+          <div className="menu-card" id="comidaRapida">
+            <img src="/img/pasta.jpg" alt="" />
+            <div className="menu-details">
+              <h2>Name product</h2>
+              <p>
+                Descripción del plato 1. Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit.
+              </p>
+              <p className="price">$15.99</p>
+            </div>
+          </div>
+
+          <div className="menu-card">
+            <img src="/img/limonada.jpg" alt="" />
+            <div className="menu-details">
+              <h2>Name product</h2>
+              <p>
+                Descripción del plato 1. Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit.
+              </p>
+              <p className="price">$15.99</p>
+            </div>
+          </div>
+
+          <div className="menu-card" id="comidaRapida">
+            <img src="/img/pasta.jpg" alt="" />
+            <div className="menu-details">
+              <h2>Name product</h2>
+              <p>
+                Descripción del plato 1. Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit.
+              </p>
+              <p className="price">$15.99</p>
+            </div>
+          </div>
+
+          <div className="menu-card">
+            <img src="/img/limonada.jpg" alt="" />
+            <div className="menu-details">
+              <h2>Name product</h2>
+              <p>
+                Descripción del plato 1. Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit.
+              </p>
+              <p className="price">$15.99</p>
+            </div>
           </div>
         </div>
-        {/*----------------*/}
-        <div
-          className="menu-card"
-          onClick={() =>
-            this.showModal(
-              "Burritos Mexicanos",
-              "/img/tacos.jpg",
-              "$15.99",
-              "Descripción del plato 1. Lorem ipsum dolor sit amet, consectetur"
-            )
-          }
-        >
-          <img src="/img/tacos.jpg" alt="" />
-          <div className="menu-details" id="comidaRapida">
-            <h2>Burritos Mexicanos</h2>
-            <p>
-              Descripción del plato 1. Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit.
-            </p>
-            <p className="price">$15.99</p>
-          </div>
-        </div>
-        {/*----------------*/}
-        <div className="menu-card">
-          <img src="/img/pizza.jpg" alt="" />
-          <div className="menu-details">
-            <h2>Name product</h2>
-            <p>
-              Descripción del plato 1. Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit.
-            </p>
-            <p className="price">$15.99</p>
-          </div>
-        </div>
-
-
-        <div className="menu-card" >
-          <img src="/img/hamburguesa.png" alt="" />
-          <div className="menu-details">
-            <h2>Name product</h2>
-            <p>
-              Descripción del plato 1. Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit.
-            </p>
-            <p className="price">$15.99</p>
-          </div>
-        </div>
-
-        <div className="menu-card" id="comidaRapida">
-          <img src="/img/pasta.jpg" alt="" />
-          <div className="menu-details">
-            <h2>Name product</h2>
-            <p>
-              Descripción del plato 1. Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit.
-            </p>
-            <p className="price">$15.99</p>
-          </div>
-        </div>
-
-        <div className="menu-card">
-          <img src="/img/limonada.jpg" alt="" />
-          <div className="menu-details">
-            <h2>Name product</h2>
-            <p>
-              Descripción del plato 1. Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit.
-            </p>
-            <p className="price">$15.99</p>
-          </div>
-        </div>
-
-        <div className="menu-card" id="comidaRapida">
-          <img src="/img/pasta.jpg" alt="" />
-          <div className="menu-details">
-            <h2>Name product</h2>
-            <p>
-              Descripción del plato 1. Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit.
-            </p>
-            <p className="price">$15.99</p>
-          </div>
-        </div>
-
-        <div className="menu-card">
-          <img src="/img/limonada.jpg" alt="" />
-          <div className="menu-details">
-            <h2>Name product</h2>
-            <p>
-              Descripción del plato 1. Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit.
-            </p>
-            <p className="price">$15.99</p>
-          </div>
-        </div>
-
-
-        </div>
-        
 
         {/*Modal*/}
         <div className="modal" id="myModal">
@@ -179,21 +192,28 @@ class ElEncuentro extends Component {
                 id="quantity"
                 min={1}
                 value={this.state.cantidad}
-                onChange={(e) => this.setState({ cantidad: Math.max(1, +e.target.value) })}
+                onChange={(e) =>
+                  this.setState({ cantidad: Math.max(1, +e.target.value) })
+                }
                 style={{
                   fontSize: "15px",
                   textAlign: "center",
                   width: "100px",
                 }}
               ></input>
-              <button className="cancel" onClick={this.closeModal}>
-                Seguir comprando
+              <button className="cancel" onClick={this.addToCart}>
+                Agregar
               </button>
+              <Link to={"/componentRestaurant/carrito"}>
               <button className="add-to-cart" onClick={this.addToCart}>
                 Agregar y Comprar
               </button>
+              </Link>
             </div>
           </div>
+        </div>
+        <div>
+          <Filtro></Filtro>
         </div>
       </div>
     );
--- Frontend/src/components/restaurant/carrito.jsx
+++ Frontend/src/components/restaurant/carrito.jsx
@@ -1,13 +1,48 @@
 import React, { Component } from "react";
+import { Link} from "react-router-dom";
+class Carrito extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      carrito: JSON.parse(localStorage.getItem("carrito")) || [],
+    };
+  }
 
-export default class Carrito extends Component{
+  eliminarProducto = (index) => {
+    // Copiar el carrito actual y eliminar el producto por su índice
+    const nuevoCarrito = [...this.state.carrito];
+    nuevoCarrito.splice(index, 1);
 
-    render(){
-        return(
-            <div><br /><br />
-                <h1>Esta es la pagina para hacer compras 🛒</h1>
-                <h2>Quitar el filtro</h2>
+    // Actualizar el estado y el carrito en el localStorage
+    this.setState({ carrito: nuevoCarrito }, () => {
+      localStorage.setItem("carrito", JSON.stringify(nuevoCarrito));
+    });
+  };
+
+  render() {
+    return (
+      <div className="contentCarrito">
+        <h1>Carrito de Compras</h1>
+        <div id="carrito-content">
+          {this.state.carrito.map((producto, index) => (
+            <div key={index} className="producto">
+              <img src={producto.imageSrc} alt={producto.title} />
+              <h2>{producto.title}</h2>
+              <p>Precio: {producto.price}</p>
+              <p>Cantidad: {producto.quantity}</p>
+              <button onClick={() => this.eliminarProducto(index)}>
+                Eliminar
+              </button>
+              <hr />
             </div>
-        );
-    }
+          ))}
+        </div>
+        <button>
+        <Link to="/componentRestaurant">Volver al Restaurante</Link>
+        </button>
+      </div>
+    );
+  }
 }
+
+export default Carrito;
